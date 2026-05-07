bd06cbf4df5516712612b581087c2247d8bb460c
hamisihussein <hamisihussein999@gmail.com>
2026-05-08 02:35:51 +0300

    Importing laravel ui bootstrap for --auth

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Auth;
 Route::get('/', function () {
     return view('booking_pages.layout');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
