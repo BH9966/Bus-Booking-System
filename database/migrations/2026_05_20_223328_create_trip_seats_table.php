3cd0794ebe251e0aaf9fb12fc8b6273bad38f68a
hamisihussein <hamisihussein999@gmail.com>
2026-05-21 01:36:23 +0300

    Add trip_seat  table for seat map and tracking available seat and sold seat as blocked seat after payment complition

---
+++ database/migrations/2026_05_20_223328_create_trip_seats_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trip_seats', function (Blueprint $table) {
+            $table->id();
+             // Trip this seat belongs to
+            $table->foreignId('trip_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            // Physical seat (A1, A2, B1...)
+            $table->foreignId('seat_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            // Booking state
+            $table->boolean('is_booked')->default(false);
+
+            // Who booked the seat (after payment)
+            $table->foreignId('booked_by')
+                ->nullable()
+                ->constrained('users')
+                ->nullOnDelete();
+            // Prevent duplicate seats per trip
+            $table->unique(['trip_id', 'seat_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trip_seats');
+    }
+};
